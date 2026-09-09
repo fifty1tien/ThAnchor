357bb236cb159490a15b592776af609584886648
fifty1tien <yellmancraft@gmail.com>
2026-09-09 07:13:46 +0700

    Fix: Perbaiki typo di base path dari /ThanAncor/ ke /ThAnchor/

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/ThanAncor/" : "/",
+  base: command === "build" ? "/ThAnchor/" : "/",
   plugins: [react()],
 }));
