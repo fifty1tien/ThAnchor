ed2f078e7ec737d3c7859cc1179d4f9f525d7dad
M. Sulthan Muzaki <yellmancraft@gmail.com>
2026-09-09 22:46:53 +0700

    Revert "Fix broken section background slices"
    This reverts commit 6f7f620ce53f1e72c58456eeb26c73a3854b9460.

--- src/components/WaveDivider.jsx
+++ src/components/WaveDivider.jsx
@@ -3,12 +3,12 @@ export default function WaveDivider({ className = "" }) {
     <div className={`wave-divider ${className}`} aria-hidden="true">
       <svg viewBox="0 0 1440 110" preserveAspectRatio="none">
         <path
-          className="wave-divider-line wave-divider-back"
-          d="M0 44 C180 90 300 5 480 43 S780 94 960 42 S1260 5 1440 43"
+          className="wave-divider-back"
+          d="M0 44 C180 90 300 5 480 43 S780 94 960 42 S1260 5 1440 43 V110 H0 Z"
         />
         <path
-          className="wave-divider-line wave-divider-front"
-          d="M0 61 C170 18 330 103 520 60 S850 17 1020 61 S1270 103 1440 58"
+          className="wave-divider-front"
+          d="M0 61 C170 18 330 103 520 60 S850 17 1020 61 S1270 103 1440 58 V110 H0 Z"
         />
       </svg>
     </div>
